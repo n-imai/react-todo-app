e8c672c65a5caac71f2953ff3e592817f1fc1d66
Nobuo Imai <8196611+n-imai@users.noreply.github.com>
2022-01-25 07:27:47 +0900

    use effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,36 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import ColorfulMessage from './components/ColorfulMessage'
 
 const App = () => {
-  const onClickButton = () =>alert()
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(true)
+
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  }
+
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  }
+
+  useEffect(() => {
+    if (num % 3 === 0) {
+      faceShowFlag || setFaceShowFlag(true);
+    }else{
+      faceShowFlag && setFaceShowFlag(false);
+    }
+  },[num])
 
   return (
     <>
       <h1 style={{ color: 'red'}}>こんにちは！</h1>
       <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
       <ColorfulMessage color="pink">元気です！</ColorfulMessage>
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <br  />
+      <button onClick={onClickSwitchShowFlag}>ON/OFF</button>
+      <p>{num}</p>
+      {faceShowFlag && <p>＼(^o^)／</p>}
     </>
   );
 };
